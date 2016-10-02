f30778a994d40fef6c24e1246f64a49840ae8a12
htchepannou <htchepannou@expedia.com>
2016-10-02 10:08:41 -0400

    fetch RSS for 1 feed

--- src/main/java/com/tchepannou/kiosk/bot/controller/RssController.java
+++ src/main/java/com/tchepannou/kiosk/bot/controller/RssController.java
@@ -3,9 +3,11 @@ package com.tchepannou.kiosk.bot.controller;
 import com.tchepannou.kiosk.bot.service.RssService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,30 @@ public class RssController {
     RssService rssService;
 
     @Async
-    @ApiOperation("Fetch data from Rss feeds")
-    @RequestMapping(value = "/fetch", method = RequestMethod.GET)
-    public void fetch() {
-        rssService.fetch();
+    @ApiOperation("Fetch data from all Rss feeds")
+    @RequestMapping(
+            value = "/fetch",
+            params = {"force"},
+            method = RequestMethod.GET
+    )
+    public void fetch(
+            @ApiParam(defaultValue = "false", allowableValues = "false,true") final String force
+    ) {
+        rssService.fetch(Boolean.parseBoolean(force));
+    }
+
+    @Async
+    @ApiOperation("Fetch data from a given Rss feed")
+    @RequestMapping(
+            value = "/fetch/feeds/{feedId}",
+            params = {"force"},
+            method = RequestMethod.GET
+    )
+    public void fetchFeed(
+            @PathVariable final String feedId,
+            @ApiParam(defaultValue = "false", allowableValues = "false,true") final String force
+    ) {
+        rssService.fetch(Long.parseLong(feedId), Boolean.parseBoolean(force));
     }
 
     @Async
