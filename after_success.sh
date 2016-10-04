934151fa138bf8b52211ee2f19fc3a74db2d64f8
htchepannou <htchepannou@expedia.com>
2016-10-04 07:25:41 -0400

    update deployment

--- after_success.sh
+++ after_success.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-wget http://roboconf.net/resources/build/settings.xml
-mvn clean deploy -q --settings settings.xml
-
 mkdir target/deploy
 cp target/kiosk-bot-1.0.jar target/deploy/kiosk-bot.jar
-cp src/install/* target/deploy/.
+
+mkdir target/deploy/install
+cp src/install/* target/deploy/install/.
